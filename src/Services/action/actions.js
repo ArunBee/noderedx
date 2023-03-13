efe3b15cca8d1ad9d8439895a38feda0de7a6843
ArunBee <aks.bsmh@gmail.com>
2023-03-13 17:58:30 +0530

    3-13

--- src/Services/action/actions.js
+++ src/Services/action/actions.js
@@ -1,1 +1,13 @@
-import React from "react";
+export const addtocart=(data) => {
+return{
+    type: "Add_To_Cart",//most important ye hume btati hai k data kis se related hai
+    data: data
+}
+}
+
+export const removetocart=(data) => {
+    return{
+        type: "Remove_To_Cart",// It must be differnet from another type
+        data: data
+    }
+    }
